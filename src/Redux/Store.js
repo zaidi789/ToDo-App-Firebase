2767b0a5590fc0f4975411c79596cd17e3ce4d75
Zaid Rafiq <zaidrafiq11@gmail.com>
2023-08-21 11:41:25 +0500

    serialize able warning resolved start working on integrating firebase to my app till now app has no errors no warnings works fine

--- src/Redux/Store.js
+++ src/Redux/Store.js
@@ -17,6 +17,11 @@ const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
   reducer: persistedReducer,
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      immutableCheck: false,
+      serializableCheck: false,
+    }),
 });
 
 export const persistor = persistStore(store);
